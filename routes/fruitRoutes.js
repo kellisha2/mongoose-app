29a1bea6d037bc52855b0a6b194a2afc66f66da1
kellisha2 <kellisha.andrew2@gmail.com>
2024-12-20 09:20:30 -0500

    made a change to delete rt

--- routes/fruitRoutes.js
+++ routes/fruitRoutes.js
@@ -44,7 +44,7 @@ router.put('/:id', async (req, res)=>{
     }
 })
 
-router.delete('/', async (req, res)=>{
+router.delete('/:id', async (req, res)=>{
     try {
         const deletedFruit = await Fruit.findByIdAndDelete(req.params.id)
         res.json(deletedFruit)
